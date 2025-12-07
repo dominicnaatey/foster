6dae96f7d0afa03ad5b4e99d79e5da0b97729f8c
dominicnaatey <dominicnaatey@gmail.com>
2025-12-07 14:48:33 +0000

    add AboutServices component to about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,13 @@
 import HeroAbout from "@/components/about/HeroAbout";
 import AboutCopy from "@/components/about/AboutInfo";
+import AboutServices from "@/components/about/AboutServices";
 
 export default function AboutPage() {
   return (
     <>
       <HeroAbout />
       <AboutCopy />
+      <AboutServices />
     </>
   );
 }
