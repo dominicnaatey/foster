1169892841afd3406f63aca3478dffad9409d52b
dominicnaatey <dominicnaatey@gmail.com>
2025-12-08 23:18:29 +0000

    add max-width constraint to paragraph text

--- components/CareSection.tsx
+++ components/CareSection.tsx
@@ -5,7 +5,7 @@ export default function CareSection() {
     <section className="bg-white px-4 py-16 md:px-16 lg:px-24 xl:px-40 ">
       <div className="max-w-6xl mx-auto">
         <div className="flex flex-col items-center gap-8 md:p-12 ">
-          <p className=" text-2xl md:text-4xl text-gray-800 text-center">
+          <p className="max-w-3xl text-2xl md:text-4xl text-gray-800 text-center">
             Trauma informed care with person-centered practices focused on reunification and/or a new sustainable beginning.
           </p>
           <div className="flex justify-center">
