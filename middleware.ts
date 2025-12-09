8facb291a6fa4912fe368898483dd45e763bce78
dominicnaatey <dominicnaatey@gmail.com>
2025-12-09 13:45:35 +0000

    add admin subdomain routing middleware

---
+++ middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const host = req.headers.get("host") || req.nextUrl.host || "";
+  if (host.startsWith("admin.")) {
+    const url = req.nextUrl.clone();
+    url.pathname = `/admin${url.pathname === "/" ? "" : url.pathname}`;
+    return NextResponse.rewrite(url);
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!_next|.*\..*).*)"],
+};
