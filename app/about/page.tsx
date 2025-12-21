0d7b6f6b7b7317efc297b915242d9a8f5c8caffa
dominicnaatey <dominicnaatey@gmail.com>
2025-12-21 08:29:35 +0000

    add GetInvolved component to about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,7 @@
 import HeroAbout from "@/components/about/HeroAbout";
 import AboutCopy from "@/components/about/AboutInfo";
 import AboutServices from "@/components/about/AboutServices";
+import GetInvolved from "@/components/about//GetInvolved";
 
 export default function AboutPage() {
   return (
@@ -8,6 +9,7 @@ export default function AboutPage() {
       <HeroAbout />
       <AboutCopy />
       <AboutServices />
+      <GetInvolved />
     </>
   );
 }
